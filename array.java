bfe1bb40571a8d1404e6e86239919ed0ffada1fc
radH3 <114068120+radH3@users.noreply.github.com>
2022-11-17 16:22:16 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class array{
+    public static void main (String[] args){
+        List<String> list=new ArrayList();
+        list.add("alpha");
+        list.add("beta");
+        list.add("gamma");
+        list.add("sigma");
+        for (String s:list){
+            System.out.println(s);
+        }
+    }
+}
